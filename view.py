87009e7d1bf7b2d53a8fc52044058cd683588d42
MaryanKindzer <kindzermar@ukr.net>
2016-03-31 13:19:19 +0300

    Maryan view commit

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 
+
 class ConsoleView(object):
     def showMatches(self,matches):
         for match in matches:
@@ -6,6 +7,7 @@ class ConsoleView(object):
                 "%s vs %s - %s:%s on %s/%s/%s in %s" %
                 (match.team1, match.team2, match.res1, match.res2,
                  match.date[0],match.date[1],match.date[2],match.country))
+
 
     def showMenu(self):
         print("""
@@ -15,14 +17,19 @@ class ConsoleView(object):
         4. Select team
         5. Exit
         """)
+
+
     def inputFromConsole(self):
         return input()
+
 
     def showIncorrectCaseMessage(self):
          print('Please try again')
 
+
     def showIncorrectTeamNameMessage(self):
         print('Unknown team name')
+
 
     def showEnterTeamMessage(self):
         print('Enter team name:', end=" ")
